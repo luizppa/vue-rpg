a28bc54e931bb2f04db156114b1ff47f463d804c
LuizPPA <luizphilippep@gmail.com>
2018-03-19 21:09:49 -0300

    Spelling fixes

--- src/main.js
+++ src/main.js
@@ -9,30 +9,30 @@ export const actions = new Vue({})
 export const master = new Vue({
   methods: {
     d20(){
-      return this.rowDice(1, 20)
+      return this.rollDice(1, 20)
     },
     d12(){
-      return this.rowDice(1, 12)
+      return this.rollDice(1, 12)
     },
     d10(){
-      return this.rowDice(1, 10)
+      return this.rollDice(1, 10)
     },
     d8(){
-      return this.rowDice(1, 8)
+      return this.rollDice(1, 8)
     },
     d6(){
-      return this.rowDice(1, 6)
+      return this.rollDice(1, 6)
     },
     d4(){
-      return this.rowDice(1, 4)
+      return this.rollDice(1, 4)
     },
     d3(){
-      return this.rowDice(1, 3)
+      return this.rollDice(1, 3)
     },
     d2(){
-      return this.rowDice(1, 2)
+      return this.rollDice(1, 2)
     },
-    rowDice(min, max) {
+    rollDice(min, max) {
       return Math.floor(Math.random() * ((max+1) - min)) + min
     }
   }
